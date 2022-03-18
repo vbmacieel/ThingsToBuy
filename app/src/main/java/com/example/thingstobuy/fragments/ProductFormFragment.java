3af0cb89085f68a98ac6825bc95295e768563738
Victor Bastos Maciel <vbastosmaciel@outlook.com.br>
2022-03-18 15:55:01 -0300

    Create a new fragment and ajusting the layout

---
+++ app/src/main/java/com/example/thingstobuy/fragments/ProductFormFragment.java
@@ -0,0 +1,24 @@
+package com.example.thingstobuy.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.thingstobuy.R;
+
+public class ProductFormFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_form, container, false);
+        return view;
+    }
+}
